7002e6be56427730c54b630fca854fc4d42d94ba
ayubazhari106 <ayubazhari106@gmail.com>
2022-08-01 19:20:09 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sunt nobis sed dignissimos saepe tempora sit dolorum cupiditate velit fugit, ea similique officiis aut accusantium repellat voluptatibus at quos in.</p>
 </body>
 </html>
